e6f700fa49bae4dd1851b64529777bfe0481ed44
avocadoaperture <67762923+avocadoaperture@users.noreply.github.com>
2020-07-04 00:20:43 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = ({
   return (
     <Layout>
       <Helmet>
-        <title>{site.siteMetadata.title}</title>
+        <title>{site.siteMetadata.title}</title> <img src="src/images/avocado aperture logo.png">
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>

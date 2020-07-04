0a49fa8815722906ea5f1bde1e7c4d8c420dc92d
avocadoaperture <67762923+avocadoaperture@users.noreply.github.com>
2020-07-04 00:42:59 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,11 @@ const IndexPage = ({
     <Layout>
       <Helmet>
         <title>{site.siteMetadata.title}</title> 
-         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>
+               <h1>Blog Postsss &darr;</h1>
+
       <h2>Blog Posts &darr;</h2>
       <div className="grids">
         {Posts}
